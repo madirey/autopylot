8946b4c985efbabc5e00770c16aaf2810d5a3540
Matt Caldwell <matt.caldwell@gmail.com>
2013-03-26 10:55:11 -0400

    add django module with get_client_ip functionality

---
+++ pyutil/django/__init__.py
@@ -0,0 +1,9 @@
+
+# from http://stackoverflow.com/questions/4581789/how-do-i-get-user-ip-address-in-django
+def get_client_ip(request):
+    x_forwarded_for = request.META.get('HTTP_X_FORWARDED_FOR')
+    if x_forwarded_for:
+        ip = x_forwarded_for.split(',')[-1].strip()
+    else:
+        ip = request.META.get('REMOTE_ADDR')
+    return ip
--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@
 from distutils.core import setup
 
 setup(name='pyutil',
-      version='1.0',
+      version='1.1',
       description='Python Utilities Collection',
       author='Matt Caldwell',
       author_email='matt.caldwell@gmail.com',
