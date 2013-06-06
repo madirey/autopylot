c9b758489c5c962f4d8dfb9a4f8c7861dad3be40
Matt Caldwell <matt.caldwell@gmail.com>
2013-06-06 09:09:00 -0400

    version rev

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@
 from distutils.core import setup
 
 setup(name='caldwellpy',
-      version='1.2',
+      version='1.3',
       description='Python Utilities Collection',
       author='Matt Caldwell',
       author_email='matt.caldwell@gmail.com',
