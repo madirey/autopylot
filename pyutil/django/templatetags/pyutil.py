8a26c3e18d5717406b80a2c41876a1f053d1a919
Matt Caldwell <matt.caldwell@gmail.com>
2013-06-06 09:37:03 -0400

    avoid KeyError ... return empty string if key not found

--- pyutil/django/templatetags/pyutil.py
+++ pyutil/django/templatetags/pyutil.py
@@ -5,4 +5,4 @@ register = template.Library()
 @register.filter(name='getkey')
 def getkey(value, arg):
     ''' Gets a value from a dict by key.  This allows keys to contain spaces, dashes, etc. '''
-    return value[arg]
+    return value.get(arg, '')
--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@
 from distutils.core import setup
 
 setup(name='caldwellpy',
-      version='1.3.5',
+      version='1.3.6',
       description='Python Utilities Collection',
       author='Matt Caldwell',
       author_email='matt.caldwell@gmail.com',
