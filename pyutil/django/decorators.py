482b5c27d316cc831a700657988535273533af34
Matt Caldwell <matt.caldwell@gmail.com>
2013-06-12 16:39:46 -0400

    allow prime_cache to be used as a decorator and contextmanager

--- pyutil/django/decorators.py
+++ pyutil/django/decorators.py
@@ -1,16 +1,20 @@
-from decorator import decorator
+from contextdecorator import ContextDecorator
 from django.core.cache import cache
 import json
 import requests
 
-def prime_cache(cache_key, data_source, timeout=60*60*24):
-    ''' default timeout 24 hours. '''
-    def prime_cache(f, *args, **kwargs):
-        data = cache.get(cache_key)
-        if not data:
-            data = json.loads(requests.get(data_source).text)
-            cache.set(cache_key, data, timeout)
-        return f()
-    return decorator(prime_cache)
+class prime_cache(ContextDecorator):
+    def __init__(self, cache_key, data_source, timeout=60*60*24):
+        self.cache_key = cache_key
+        self.data_source = data_source
+        self.timeout = timeout
 
-#@prime_cache('ships', 'http://54.235.218.58/ships?format=json')
+    def __enter__(self):
+        data = cache.get(self.cache_key)
+        if not data:
+            data = json.loads(requests.get(self.data_source).text)
+            cache.set(self.cache_key, data, self.timeout)
+        return self
+
+    def __exit__(self, type, value, traceback):
+        return False
