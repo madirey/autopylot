e7ac895ae1938ff9ed5ef1f7e372f0801c692222
Matt Caldwell <matt.caldwell@gmail.com>
2013-06-12 22:34:02 -0400

    add fault tolerance, new request timeout feature

--- setup.py
+++ setup.py
@@ -3,10 +3,17 @@
 from distutils.core import setup
 
 setup(name='caldwellpy',
-      version='1.4.1',
+      version='1.4.9',
       description='Python Utilities Collection',
       author='Matt Caldwell',
       author_email='matt.caldwell@gmail.com',
       url='https://github.com/mattcaldwell/pyutil',
-      packages=['pyutil', 'pyutil.django', 'pyutil.django.templatetags']
+      packages=['pyutil', 'pyutil.django', 'pyutil.django.templatetags'],
+      install_requires=[
+          'Django==1.4.5',
+          'contextdecorator==0.10.0',
+          'mock==1.0.1',
+          'requests==1.2.3',
+          'wsgiref==0.1.2',
+      ]
 )
